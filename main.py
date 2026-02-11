844a0465c8248501145ac558a5478d45f4b1127e
playwithsakthigaming-sketch <playwithsakthigaming@gmail.com>
2026-02-11 14:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def serve_file(filename):
 
 
 def run_flask():
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 
